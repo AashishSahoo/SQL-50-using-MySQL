db857cdaeff407793e66c7cf4519d71b5370e9b9
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2024-02-15 05:28:59 +0530

    Time: 540 ms (90.81%), Space: 0B (100.00%) - LeetHub

---
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,11 @@
+
+#570. Managers with at Least 5 Direct Reports
+# Write your MySQL query statement below
+
+select e1.name
+from Employee e1
+join Employee  e2
+on e1.id=e2.managerId 
+group by e1.id
+having count(e2.id)>=5
+
