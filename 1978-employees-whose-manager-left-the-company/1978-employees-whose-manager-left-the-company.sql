b37caa56be0df6788d50c636c6d931068af239c9
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2024-02-15 05:34:45 +0530

    Time: 500 ms (99.86%), Space: 0B (100.00%) - LeetHub

---
+++ 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
@@ -0,0 +1,12 @@
+#1978. Employees Whose Manager Left the Company
+# Write your MySQL query statement below
+
+
+select employee_id
+from Employees
+where salary<30000 
+  and  manager_id not in 
+  (select employee_id from Employees)
+group by employee_id
+order by employee_id  
+
